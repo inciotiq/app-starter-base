d46934b55ffc386a15b97730750f0bf2932511fe
inci <inci@iotiq.net>
2023-06-12 11:39:55 +0300

    test commit 2

--- src/main/java/com/iotiq/application/Application.java
+++ src/main/java/com/iotiq/application/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        // some changes
+        // some changes -
         SpringApplication.run(Application.class, args);
     }
 
