00ec5e0b8af1b70b31c8a5624160b443422f28f6
inci <inci@iotiq.net>
2023-06-16 15:36:51 +0300

    reformat SecurityConfiguration file

--- src/main/java/com/iotiq/application/config/SecurityConfiguration.java
+++ src/main/java/com/iotiq/application/config/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import com.iotiq.user.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,20 +13,17 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-//@Import(SecurityProblemSupport.class)
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
     private final TokenProvider tokenProvider;
 
-//    private final CorsFilter corsFilter;
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/*")
-                        .permitAll().anyRequest().authenticated())
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/v1/auth/*").permitAll()
+                        .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .apply(new JWTConfigurer(tokenProvider));
         return http.build();
