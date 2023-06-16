a6f472d775f214acc35a98021c77d548b87829de
inci <inci@iotiq.net>
2023-06-16 15:45:53 +0300

    add enableMethodSecurity annotation

--- src/main/java/com/iotiq/application/config/SecurityConfiguration.java
+++ src/main/java/com/iotiq/application/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.iotiq.user.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,6 +14,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
